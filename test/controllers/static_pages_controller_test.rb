835488dcb42a8f909510103736934543c705af85
shakil-fg <md.ahnaf.tahmid.shakil@framgia.com>
2015-10-08 16:53:59 +0600

    Static pages done!

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "should get home title" do
+    get :home
+    assert_select "title", "Home Project-test4"
+  end
+
+  test "should get help title" do
+    get :help
+    assert_select "title", "Help Project-test4"
+  end
+  
+   test "should get about title" do
+    get :about
+    assert_select "title", "About Project-test4"
+  end
+
+end
