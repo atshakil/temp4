284e93ec30fda20056cf9ed1c2b096c33f800ec2
shakil-fg <md.ahnaf.tahmid.shakil@framgia.com>
2015-10-12 12:53:46 +0600

    Finished chapter: Filling in the layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,31 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Project-test4"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Project-test4"
   end
   
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Project-test4"
   end
   
-  test "should get home title" do
-    get :home
-    assert_select "title", "Project-test4"
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Project-test4"
   end
-
-  test "should get help title" do
-    get :help
-    assert_select "title", "Help Project-test4"
-  end
-  
-   test "should get about title" do
-    get :about
-    assert_select "title", "About Project-test4"
-  end
-
 end
