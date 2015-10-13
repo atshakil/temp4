e919cadfa006a28278282d12f1a8487c5deed85f
shakil-fg <md.ahnaf.tahmid.shakil@framgia.com>
2015-10-13 15:50:03 +0600

    Chapter 6 done: Made basic user model with secured password

---
+++ test/models/user_test.rb
@@ -0,0 +1,71 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  def setup
+    #@user = User.new(name: "Test User", email: "some@email.com")
+    @user = User.new(name: "Michael Hartl", email: "mhartl@example.com", password: "foobar", password_confirmation: "foobar")
+  end
+  
+  test "~should be valid" do
+    assert @user.valid?
+  end
+
+  test "name shouldn't be empty" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email shouldn't be empty" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should've reasonable length" do
+    @user.name = 'n'*51
+    assert_not @user.valid?
+  end
+
+  test "email should've reasonable length" do
+    @user.email = 'n'*246+'@gmail.com'
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+    foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+
+end
+
+
+
